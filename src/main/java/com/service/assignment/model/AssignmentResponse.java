ed2ef0a46d321b62d146fcf8a112b2eba2207846
vibhaanshu <vibhaanshu@yahoo.com>
2019-06-23 15:17:03 +0530

    changed value

--- src/main/java/com/service/assignment/model/AssignmentResponse.java
+++ src/main/java/com/service/assignment/model/AssignmentResponse.java
@@ -2,8 +2,8 @@ package com.service.assignment.model;
 
 public class AssignmentResponse {
 	
-	Object[][] assignList = { {"Date", "Assignment","Submission", "Average"}, {"1",10,0,0},
-			{"2",10,0,0},{"3",10,0,0},{"4",10,0,0}};
+	Object[][] assignList = { {1, 10,20, 30}, {1,10,0,0},
+			{2,10,0,0},{3,10,0,0},{3,10,0,0}};
 
 	public Object[][] getAssignList() {
 		return assignList;
